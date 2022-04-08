feb187e3d6bf9bad874c1503dd4bb41355216728
martinp@n0r.dk <martinp@n0r.dk>
2022-04-08 09:38:03 +0200

    Project Upload

---
+++ CreateUserInAD/Models/ADUserContext.cs
@@ -0,0 +1,24 @@
+﻿namespace CreateUserInAD.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Linq;
+    public class ADContext : DbContext
+    {
+        // Your context has been configured to use a 'WebsiteContext' connection string from your application's 
+        // configuration file (App.config or Web.config). By default, this connection string targets the 
+        // 'Website.Models.WebsiteContext' database on your LocalDb instance. 
+        // 
+        // If you wish to target a different database and/or database provider, modify the 'WebsiteContext' 
+        // connection string in the application configuration file.
+        public ADContext() : base("name=ADContext")
+        {
+        }
+
+        // Add a DbSet for each entity type that you want to include in your model. For more information 
+        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        public virtual DbSet<User> Users { get; set; }
+    }
+}
---
+++ CreateUserInAD/Models/User.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CreateUserInAD.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        [Required, DisplayName("First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        public string Initials { get; set; }
+        [Required, DisplayName("Last name")]
+        public string Lastname { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [DisplayName("Street address")]
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        [DisplayName("Zip Code")]
+        public string ZipCode { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string Department { get; set; }
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public string Telephone { get; set; }
+        [DisplayName("Job Title")]
+        public string JobTitle { get; set; }
+        public string Company { get; set; }
+        public string OU { get; set; }
+
+    }
+}
